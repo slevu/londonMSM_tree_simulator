a56886518252ec2bd3a421b41d3387e7aed404e6
slevu <s.levu@laposte.net>
2016-02-23 23:54:23 +0000

    loop for hivclustering on sim tree

--- hivclustering.R
+++ hivclustering.R
@@ -14,92 +14,51 @@ getwd()
 # test <- read.csv(file = "~/Documents/phylo-uk/source/subUKogC_noDRM_151202_ucsdTN93.csv")
 # dim(test)/12164 *12164 /2
 # head(test)
-
-dsimtree <- readRDS("data/simtree_dist.rds")
-## normalize
-d <- round(dsimtree / (max(dsimtree) - min(dsimtree)), 4)
-rm(dsimtree)
-
-m <- as.matrix(d)
-dim(m)
-
-lt <- m[lower.tri(m, diag = FALSE)]
-dim(lt)
-library(reshape2)
-system.time(
-  el <- melt(lt)
-)
-head(el)
-## get rid of distance > k = mean
-k <- round(mean(d),1)
-dd <- as.dist(d[d < k])
-str(dd)
-vect <- d < k
-sum(vect)
-as.dist(yoursymmetricmatrix[k, k])
-
-rm(d)
-str(dd)
-m <- as.matrix(d)
-dim(m)
-mm <- m[12160:12164, 12160:12164]
-dd <- as.dist(mm)
-
- require(igraph)
- g <- graph.adjacency(mm, mode = "lower", diag = FALSE)
- plot(g)
- get.edgelist(g)
-#     ?graph.adjacency
-melt(mm)
-
-mm[lower.tri(mm, diag = FALSE)]
-a <- m[ mm<0.3 ]
-str(a)
-ucsd_hivclust <- function(d){
-  if ( class(d) == "dist" ){
-    ## make a csv edge list (ID1, ID2, distance)
-     ## convert matrix to edge list
-     ## essai igraph
-#     require(igraph)
-#     g <- graph(d)
-#     edge <- get.edgelist(g)
-#     ?graph.adjacency
-#     
-      m <- as.matrix(d)
-      edges <- matrix(0, nrow = nrow(m)*nrow(m), ncol = 3)
-      system.time(
-       for (i in 1:nrow(m)) {
-        for (j in 1:ncol(m)) {
-         edges[(i-1)*nrow(m)+j , ] <- c(rownames(m)[i], 
-                         rownames(m)[j], 
-                         m[i,j])
-      }
-         # progress
-         print(i)
-         # update GUI console
-         flush.console() }
-      )
-      warnings()
-      ## 37s for 3 rows = 6s by row, 6*12164 = 72984 / 60 /60 = 20h !!!
-      ## 
-    colnames(edges) <- c('ID1', 'ID2', 'distance')
-    df <- as.data.frame(edges, stringsAsFactors = F)
-    df$distance <- as.numeric(df$distance)
-    
-    } else stop("d is not a distance matrix")
+if(FALSE){
   
-#     head(edges)
-#     str(df)
-#     
-    ## write csv without rownames !
-    inputCSV <- paste(tempdir(), "/input.csv", sep = "")
-    write.csv(df, file = inputCSV, row.names = FALSE )
+  dsimtree <- readRDS("data/simtree_dist.rds")
+  ## normalize
+  dsimtree <- round(dsimtree / 
+                      (max(dsimtree) - min(dsimtree)), 4)
+  str(dsimtree)
 
-  ## threshold
-  thr <- 0.9 # or for (thr in c(0.5)){}
+  ## as matrix
+  m <- as.matrix(dsimtree)
+  dim(m)
+  # m <- m[1:6,1:6]
+
+  ## keep only the lower triangle by 
+  ## filling upper with NA
+  m[upper.tri(m, diag=TRUE)] <- NA
+
+  library(reshape2)
+  system.time(
+    el <- melt(m, na.rm = TRUE)
+  )
+  # head(el)
+  str(el)
+  colnames(el) <- c('ID1', 'ID2', 'distance')
+  ## omit the NA values
+  # el <- na.omit(el)
+
+  ## get rid of distance > k = mean
+  k <- round(mean(el$distance),1)
+  subel <- el[el$distance < k,]
+  head(subel)
+
+  ## write csv without rownames !
+  inputCSV <- paste(tempdir(), "/input.csv", sep = "")
+  write.csv(subel, file = inputCSV, row.names = FALSE )
   
+  rm(m, dsimtree, subel, el)
+}
+
+####---- loop threshold
+
+for (thr in (3:6)/10){
+  inputCSV <- paste(tempdir(), "/input.csv", sep = "")
   ## output
-  outputCSV <- paste(tempdir(), "/input_", thr,".csv", sep = '')
+  outputCSV <- paste("ucsd_hivclustering_output_", thr,".csv", sep = '')
   ## parms
   parms <- paste("-t", thr, "-f plain")
   ## full path needed 
@@ -109,24 +68,27 @@ ucsd_hivclust <- function(d){
   cmd_hivclustering
   
   system(cmd_hivclustering)
-  
-  read.csv(outputCSV)
 }
+    
+  cl <- read.csv(outputCSV)
 
+head(cl)
+
+##### surplus #####
 # thr <- 0.5
-for (thr in c(0.5)){
-  
-  inputCSV <- paste(getwd(), "/source/","subUKogC_noDRM_151202_ucsdTN93.csv", sep = '')
-  outputCSV <- paste(getwd(), "/data/","subUKogC_noDRM_151202_UCSDclust_", thr,".csv", sep = '')
-  
-  parms <- paste("-t", thr/100, "-f plain")
-  
-  ## full path needed 
-  exec <- "~/Documents/softwares/hivclustering/scripts/hivnetworkcsv"
-  ## command
-  cmd_hivclustering <- paste(exec, " -i", inputCSV, "-c", outputCSV, parms )
-  cmd_hivclustering
-  
-  system(cmd_hivclustering)
-  
-}
+# for (thr in c(0.5)){
+#   
+#   inputCSV <- paste(getwd(), "/source/","subUKogC_noDRM_151202_ucsdTN93.csv", sep = '')
+#   outputCSV <- paste(getwd(), "/data/","subUKogC_noDRM_151202_UCSDclust_", thr,".csv", sep = '')
+#   
+#   parms <- paste("-t", thr/100, "-f plain")
+#   
+#   ## full path needed 
+#   exec <- "~/Documents/softwares/hivclustering/scripts/hivnetworkcsv"
+#   ## command
+#   cmd_hivclustering <- paste(exec, " -i", inputCSV, "-c", outputCSV, parms )
+#   cmd_hivclustering
+#   
+#   system(cmd_hivclustering)
+#   
+# }
