b3125d221ea7fc76a5183b4eed2b341ffe5d723a
slevu <s.levu@laposte.net>
2016-03-17 11:49:56 +0000

    restrict outdegree analyses to most recent years

--- test_size.vs.covar_SA_uk.R
+++ test_size.vs.covar_SA_uk.R
@@ -3,14 +3,19 @@
 source("functions.R")
        
 out <-  readRDS(file = "../phylo-uk/data/outdegree.rds")
+head(out)
+## restriction in time
+origin <- 2003
+out <- out[out$ydiag > origin,]
 
 fit <- lm(outdegree ~ agediag + sqrt(cd4) + ydiag , data = out) 
 summary(fit)
 str(out)
 ### plot (input l as a list)
-size.vs.covar(l = list(out), depvar = "outdegree", indepvar = c("agediag", "cd4", "ydiag"))
+#size.vs.covar(l = list(out), depvar = "outdegree", indepvar = c("agediag", "cd4", "ydiag"))
 
 ### reg
 lm_model <- "outdegree ~ agediag + sqrt(cd4) + ydiag"
 reg.sum(ls = list(out), reg = lm, model = lm_model)
+
 
