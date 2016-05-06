6424009163af2e0f1c96df286e789351fb8138e9
slevu <s.levu@laposte.net>
2016-05-06 18:58:48 +0100

    foreach runs of sims

---
+++ run_par_sims.R
@@ -0,0 +1,20 @@
+
+library(doParallel)
+registerDoParallel(cores = 4)
+
+system.time(
+foreach(i = 1:4) %dopar% {
+  source('Erik_code/model0-simulateBaseline0.R')
+}
+)
+# getDoParWorkers()
+# getDoParName()
+?detectCores()
+
+##- test nested
+foreach(k = 1:3, .combine = cbind) %:% 
+foreach(i = 1:4, .combine = c) %dopar% {
+  Sys.getpid() # Sys.info()[['nodename']]
+}
+
+?"foreach"
