f9030cf7dbb54465c2943acfe55f8713476d2e64
slevu <s.levu@laposte.net>
2016-07-06 14:49:44 +0100

    update

--- 5_summary_stats.R
+++ 5_summary_stats.R
@@ -7,10 +7,10 @@ library(cowplot)
 
 ##---- load data ----
 cw_Baseline0 <- readRDS(file = "data/sim_ucsd_results2/list.sim.clus-outdeg.Baseline0.rds" )
-cw <- cw_Baseline0[-c(2)]
+cw <- cw_Baseline0[c('SA', '0.001', '0.005', '0.015', '0.05')]
 # names(cw)
 ##- rbind table
-cw_bind <- lapply(cw, function(x) do.call(rbind, x[1:100]))
+cw_bind <- lapply(cw, function(x) do.call(rbind, x[1:5]))
 # str(cw_bind)
 
 ##---- plan ----
@@ -71,11 +71,104 @@ sapply(cw[-1], function(x) lapply(x, function(df) aggregate(df$size, by = list("
 sapply(cw_bind[-1], function(x) cor(x$size, x$nbhsize))
 str(cw_bind)
 
+##--- plots ---
+##- long table with value = od, size ...; method = SA, CL, NB; thr = ..
 ##- for plots
-a <- melt(cw_bind[-1], id.vars = 'id', measure.vars = c('size','nbhsize'))
+source('functions.R')
+molten <- function(cw_bind){
+  .sa <- melt( cw_bind[[1]], id.vars = c('id', 'stage', 'age', 'risk'),  measure.vars = 'outdegree')
+  # head(.sa)
+  .thr <- melt(cw_bind[-1], id.vars = c('id', 'stage', 'age', 'risk'), measure.vars = c('size', 'nbhsize'))
+  # head(.thr)
+  tot <- rbind( cbind(.sa, "L1" = 'NA'), .thr )
+  # head(tot)
+  return(tot)
+}
 
-str(a)
-head(a)
+##- winsorized (with function #1)
+cw_bind_win1 <- lapply(cw_bind, function(x){
+  if ( any(grepl('outdegree', names(x))) ) {
+    x[, 'outdegree'] <- winsorize( x[, 'outdegree'], 0.02 )
+    return(x)
+  } else {
+    x[, 'size'] <- winsorize( x[, 'size'], 0.02 )
+    x[, 'nbhsize'] <- winsorize( x[, 'nbhsize'], 0.02 )
+    return(x)
+  }
+})
 
-g1 <- ggplot(a, aes(L1, value, color = variable)) + geom_boxplot() + facet_wrap(~variable) 
-g1
+##- winsorized (with function #2)
+cw_bind_win2 <- lapply(cw_bind, function(x){
+  if ( any(grepl('outdegree', names(x))) ) {
+    x[, 'outdegree'] <- winsorize2( x[, 'outdegree'])
+    return(x)
+  } else {
+    x[, 'size'] <- winsorize2( x[, 'size'])
+    x[, 'nbhsize'] <- winsorize2( x[, 'nbhsize'])
+    return(x)
+  }
+})
+
+##- no minimum value
+cw_bind_nozero <- lapply(cw_bind, function(x){
+  if ( any(grepl('outdegree', names(x))) ) {
+    x[x[, 'outdegree'] == 0, 'outdegree'] <- NA
+    return(x)
+  } else {
+    x[x[, 'size'] == 1 , 'size'] <- NA
+    x[x[, 'nbhsize'] == 0, 'nbhsize'] <- NA
+    return(x)
+  }
+})
+
+# str(cw_bind_nozero)
+summary(cw_bind[[2]]$nbhsize)
+summary(cw_bind_win[[2]]$nbhsize)
+
+tot <- molten(cw_bind)
+tot_win <- molten(cw_bind_win)
+tot_win2 <- molten(cw_bind_win2)
+tot_nozero <- molten(cw_bind_nozero)
+
+# str(tot_win)
+
+##- function plot
+gg <- function(x , var , lbl, tran = identity){
+  g1 <- ggplot(x, aes_string(var, 'tran(value)', color = 'variable')) + 
+    geom_boxplot() + 
+    facet_wrap( ~ variable + L1, scales = "free_y", ncol = 3, labeller= labeller(variable = c(outdegree = "Out-degree", size = "Cluster size", nbhsize = "Neighborhood size")))  + 
+    xlab(lbl) + ylab("Value") + 
+    theme(legend.position="none") +
+    theme(strip.background = element_blank()) +
+    background_grid()
+  g1
+}
+
+##- apply
+gg(tot, 'factor(risk)', 'Risk level')
+gg(tot_win, 'factor(risk)', 'Risk level')
+gg(tot_win2, 'factor(risk)', 'Risk level')
+gg(tot_nozero, 'factor(risk)', 'Risk level')
+
+gg(tot, 'factor(age)', 'Age category')
+gg(tot_win, 'factor(age)', 'Age category')
+gg(tot_nozero, 'factor(age)', 'Age category')
+
+gg(tot, 'factor(stage)', 'Infection stage')
+gg(tot_win, 'factor(stage)', 'Infection stage')
+gg(tot_nozero, 'factor(stage)', 'Infection stage')
+
+###
+# http://stackoverflow.com/questions/5677885/ignore-outliers-in-ggplot2-boxplot
+# create a dummy data frame with outliers
+df = data.frame(y = c(-100, rnorm(100), 100))
+
+# create boxplot that includes outliers
+p0 = ggplot(df, aes(y = y)) + geom_boxplot(aes(x = factor(1)))
+
+
+# compute lower and upper whiskers
+ylim1 = boxplot.stats(df$y)$stats[c(1, 5)]
+
+# scale y limits based on ylim1
+p1 = p0 + coord_cartesian(ylim = ylim1*1.05)
--- functions.R
+++ functions.R
@@ -840,3 +840,23 @@ winsorize <- function (x, fraction=.05)
   x
 }
 #---------------------------------#
+
+#---------------------------------#
+##---- winsorize outliers 2 -----
+#---------------------------------#
+winsorize2 <- function (x, multiple=3)
+{
+  if(length(multiple) != 1 || multiple <= 0) {
+    stop("bad value for 'multiple'")
+  }
+  med <- median(x)
+  y <- x - med
+  sc <- mad(y, center=0) * multiple
+  y[ y > sc ] <- sc
+  y[ y < -sc ] <- -sc
+  y + med
+}
+#---------------------------------#
+
+#----------------------------------#
+
